206f71f8efd57f848592f811ce2b8ab572c4a85f
ruanpienaar <ruan800@gmail.com>
2016-06-23 14:05:22 +0100

    app and sup include

--- app_sup.erl
+++ app_sup.erl
@@ -11,6 +11,8 @@
 %% Helper macro for declaring children of supervisor
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
 
+-include("{{appid}}.hrl").
+
 %% ===================================================================
 %% API functions
 %% ===================================================================
