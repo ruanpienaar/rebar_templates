08454cdbfc4a814615353b0c0e835588cb2779f7
ruanpienaar <ruan800@gmail.com>
2018-03-23 10:32:17 +0000

    ..

--- testing/ctsuite.erl
+++ testing/ctsuite.erl
@@ -1,124 +1,67 @@
 -module({{name}}_SUITE).
-%% Note: This directive should only be used in test suites.
--compile(export_all).
+-export([
+    all/0,
+    suite/0,
+    groups/0,
+    init_per_suite/1,
+    end_per_suite/1,
+    group/1,
+    init_per_group/2,
+    end_per_group/2,
+    init_per_testcase/2,
+    end_per_testcase/2
+]).
+% Tests
+-export([
+    my_test_case/0,
+    my_test_case/1
+]).
 -include_lib("common_test/include/ct.hrl").
-%%%.
-%%%' CALLBACKS
 
-%% @doc returns list of tuples to set default properties for the suite.
--spec suite() -> Config
-  when
-      Config :: proplist().
-suite() ->
-  [{timetrap,{minutes,10}}].
-
-%% @doc runs initialization before matching test suite is executed.
-%%      This function may add key/value pairs to Config.
--spec init_per_suite(Config0) ->
-  Config1 | {skip, Reason} | {skip_and_save, Reason, Config1}
-  when
-      Config0 :: proplist(),
-      Config1 :: proplist(),
-      Reason :: term().
-init_per_suite(Config) ->
-  Config.
-
-%% @doc runs cleanup after matching test suite is executed.
--spec end_per_suite(Config0) -> ok | {save_config, Config1}
-  when
-      Config0 :: proplist(),
-      Config1 :: proplist().
-end_per_suite(_Config) ->
-  ok.
-
-%% @doc runs initialization before matching test group is executed.
--spec init_per_group(GroupName, Config0) ->
-  Config1 | {skip, Reason} | {skip_and_save, Reason, Config1}
-  when
-      GroupName :: string(),
-      Config0 :: proplist(),
-      Config1 :: proplist(),
-      Reason :: term().
-init_per_group(_GroupName, Config) ->
-  Config.
-
-%% @doc runs cleanup after matching test group is fully executed.
--spec end_per_group(GroupName, Config0) -> ok | {save_config, Config1}
-  when
-      GroupName :: string(),
-      Config0 :: proplist(),
-      Config1 :: proplist().
-end_per_group(_GroupName, _Config) ->
-  ok.
-
-%% @doc runs initialization before matching test case. Should not alter or
-%%      remove any key/value pairs to the Config, but may add to it.
--spec init_per_testcase(TestCase, Config0) ->
-  when
-      TestCase :: atom(),
-      Config0 :: proplist(),
-      Config1 :: proplist(),
-      Reason :: term().
-init_per_testcase(_TestCase, Config) ->
-  Config.
-
-%% @doc runs cleanup for matching test case.
--spec end_per_testcase(TestCase, Config0) ->
-  ok | {save_config, Config1} | {fail, Reason}
-  when
-      TestCase :: atom(),
-      Config0 :: proplist(),
-      Config1 :: proplist(),
-      Reason :: term().
-end_per_testcase(_TestCase, _Config) ->
-  ok.
-
-%% @doc returns list of test case group definitions.
--spec groups() -> [Group]
-  when
-      Group :: {GroupName, Properties, Members},
-      GroupName :: atom(),
-      Properties :: [parallel | sequence | Shuffle | {RepeatType, N}],
-      Members :: [Group | {group, GroupName} | TestCase],
-      TestCase :: atom(),
-      Shuffle :: shuffle | {shuffle, Seed},
-      Seed :: {integer(), integer(), integer()},
-      RepeatType :: repeat | repeat_until_all_ok | repeat_until_all_fail |
-        repeat_until_any_ok | repeat_until_any_fail
-      N :: integer() | forever.
-groups() ->
-  [].
-
-%% @doc returns list of tests (group or testcase) to be run for all of suite.
--spec all() -> Tests | {skip, Reason}
-  when
-      Test :: {group, GroupName} | TestCase,
-      Tests :: [Test],
-      GroupName :: atom(),
-      TestCase :: atom(),
-      Reason :: term().
+% Returns a list of all test cases and groups in the suite. (Mandatory)
 all() ->
-  [my_test_case].
+    [my_test_case].
 
-%%%.
-%%%'   TESTCASES
+% Information function used to return properties for the suite. (Optional)
+suite() ->
+    [].
+    
+% For declaring test case groups. (Optional)
+groups() ->
+    [].
 
-%% @doc returns list of tuples to set info properties for the test case.
--spec my_test_case() -> Info
-  when
-      Info :: proplist().
+% Suite level configuration function, executed before the first test case. (Optional)
+init_per_suite(Config) ->
+    Config.
+
+% Suite level configuration function, executed after the last test case. (Optional)
+end_per_suite(_Config) ->
+    ok.
+    
+% Information function used to return properties for a test case group. (Optional)
+group(_GroupName) ->
+    [].
+
+% Configuration function for a group, executed before the first test case. (Optional)
+init_per_group(_GroupName, Config) ->
+    Config.
+
+% Configuration function for a group, executed after the last test case. (Optional)
+end_per_group(_GroupName, _Config) ->
+    ok.
+    
+% Configuration function for a testcase, executed before each test case. (Optional)
+init_per_testcase(_TestCase, Config) ->
+    Config.
+
+% Configuration function for a testcase, executed after each test case. (Optional)
+end_per_testcase(_TestCase, _Config) ->
+    ok.
+    
+% Information function that returns a list of test case properties. (Optional)
 my_test_case() ->
-  [].
+    [].
 
-%% @doc the test function.
-@spec TestCase(Config0) ->
-        ok | exit() | {skip, Reason} | {comment, Comment} |
-        {save_config, Config} | {skip_and_save, Reason, Config1}
-  when
-  Config0 :: Config1 :: proplist(),
-      Reason :: term(),
-      Comment :: term()
+% The test case function.
 my_test_case(_Config) ->
-  ok.
-%%%.
-%%% vim: set filetype=erlang tabstop=2 foldmarker=%%%',%%%. foldmethod=marker:
+    ok.
